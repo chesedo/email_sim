f76f45632e37b84d7a9397d481ca31655089bc32
chesedo <pieter@chesedo.me>
2025-02-14 12:18:23 +0200

    Store containers for easier access

--- test_time_control.py
+++ test_time_control.py
@@ -5,7 +5,6 @@ from datetime import datetime
 import requests
 import time
 from python_on_whales import DockerClient
-import tempfile
 from pathlib import Path
 
 class DockerTimeController:
@@ -20,7 +19,7 @@ class DockerTimeController:
         self.env_path.write_text(f"FAKETIME={faketime_timestamp}\n")
 
         self.docker = DockerClient()
-        self.container = None
+        self.containers = {}
 
     def start_container(self):
 
@@ -29,21 +28,29 @@ class DockerTimeController:
             wait=True
         )
 
-        # Get container instance
+        # Convert list of containers to a map by service name
         containers = self.docker.compose.ps()
         if not containers:
             raise RuntimeError("No containers started")
-        self.container = containers[0]
 
+        # Map of service_name -> container
+        self.containers = {
+            container.name.split('-')[1]: container
+            for container in containers
+        }
+
+        print("Available services:", list(self.containers.keys()))
+
+    def get_time(self):
         # Get the mapped port
-        port_mappings = self.container.network_settings.ports["8080/tcp"]
+        timeserver = self.containers['timeserver']
+        port_mappings = timeserver.network_settings.ports["8080/tcp"]
         if not port_mappings:
             raise RuntimeError("Could not find mapped port")
 
-        self.host_port = port_mappings[0]["HostPort"]
+        host_port = port_mappings[0]["HostPort"]
 
-    def get_container_time(self):
-        response = requests.get(f"http://localhost:{self.host_port}/time")
+        response = requests.get(f"http://localhost:{host_port}/time")
         return datetime.strptime(response.json()['current_time'], '%Y-%m-%d %H:%M:%S')
 
     def cleanup(self):
@@ -63,7 +70,7 @@ def time_controlled_container():
 
 def test_time_control(time_controlled_container):
     # Get the container's current time
-    container_time = time_controlled_container.get_container_time()
+    container_time = time_controlled_container.get_time()
     expected_time = datetime(2024, 8, 1, 12, 0, 0)
 
     # Assert that the time was set correctly (allowing for a small difference due to execution time)
@@ -72,6 +79,6 @@ def test_time_control(time_controlled_container):
 
     # Wait for 5 seconds
     time.sleep(5)
-    new_time = time_controlled_container.get_container_time()
+    new_time = time_controlled_container.get_time()
     time_difference = abs((new_time - container_time).total_seconds())
     assert time_difference == 5, f"Time difference should be 5 seconds, but was {time_difference}"
